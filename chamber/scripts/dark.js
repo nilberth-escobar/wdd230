b1a7b67161c3ceba3d2d65b6dcf3e93b4ce210ee
nilberth-escobar <nilberrhescobar@gamil.com>
2023-10-14 22:19:30 -0600

    dark fixed

--- chamber/scripts/dark.js
+++ chamber/scripts/dark.js
@@ -1,22 +1,22 @@
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
-const eventsNow = document.querySelector(".events-now");
-const h4 = document.querySelector("h4");
+//const eventsNow = document.querySelector(".events-now");
+//const h4 = document.querySelector("h4");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🕶️")) {
 		main.style.background = "#2f4858";
 		main.style.color = "#fff";
-        eventsNow.style.color = "#2f4858";
+        /*eventsNow.style.color = "#2f4858";
         h4.style.backgroundColor = "#2f4858";
-        h4.style.color = "#fff";
+        h4.style.color = "#fff";*/
 		modeButton.textContent = "🔆";
 	} else {
 		main.style.background = "#eee";
 		main.style.color = "#000";
 		modeButton.textContent = "🕶️";
-        eventsNow.style.color = "#fff";
+        /*eventsNow.style.color = "#fff";
         h4.style.backgroundColor = "#fff";
-        h4.style.color = "#2f4858";
+        h4.style.color = "#2f4858";*/
 	}
 });
