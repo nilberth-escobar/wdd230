5441f042e4d7b6c393390100a5739b654fe32f6c
nilberth-escobar <nilberrhescobar@gamil.com>
2023-11-10 21:04:55 -0600

    directory html, css, json and script created

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,18 @@
+const gridContainer = document.querySelector('.grid-container');
+const listContainer = document.querySelector('.list-container');
+const gridToggle = document.querySelector('.toggle-view .grid');
+const listToggle = document.querySelector('.toggle-view .list');
+
+gridToggle.addEventListener('click', function() {
+  gridContainer.style.display = 'grid';
+  listContainer.style.display = 'none';
+  gridToggle.classList.add('l-active');
+  listToggle.classList.remove('l-active');
+});
+
+listToggle.addEventListener('click', function() {
+  gridContainer.style.display = 'none';
+  listContainer.style.display = 'block';
+  gridToggle.classList.remove('l-active');
+  listToggle.classList.add('l-active');
+});
